b8d69e3e487a7ef1ee3696a0ba2e3c5fd14156e9
Darielquinta <darielquinta@gmail.com>
2023-01-14 18:29:47 +0000

    Initial commit.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if(birthYear )
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Dariel';
+calcAge(2006);
+// printAge();

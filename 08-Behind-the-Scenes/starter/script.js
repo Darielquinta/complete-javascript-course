dfe6b426583250cff0cdee4e1e8491e862f71aa2
Darielquinta <darielquinta@gmail.com>
2023-01-14 18:30:00 +0000

    Additional commit.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -4,10 +4,27 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
     console.log(output);
 
-    if(birthYear )
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable.
+      const firstName = 'Steven';
+
+      // Reassigning outer scope's variable
+      output = 'NEW OUTPUT';
+
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
   }
   printAge();
 
@@ -15,5 +32,5 @@ function calcAge(birthYear) {
 }
 
 const firstName = 'Dariel';
-calcAge(2006);
+calcAge(1984);
 // printAge();
